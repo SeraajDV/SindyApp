7aa53c63a0860d50d8bc2e1fdce151372ccb5e06
SeraajDV <seraajdevilliers@gmail.com>
2021-09-08 09:22:55 +0200

    init

--- Server.js
+++ Server.js
@@ -26,7 +26,7 @@ app.get('/search/:term/:media', async(req, res) => {
 app.use(express.static(path.join(__dirname, 'frontend/build')))
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname+'/frontend/build/index.html'))
+    res.sendFile(path.resolve(__dirname,'frontend', 'build', 'index.html'))
 })
 
 const PORT = process.env.PORT || 8080;
